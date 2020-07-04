ce012d15440fd233ae172337967e8faa48c074fb
Jesse Miller <5042689+theshape@users.noreply.github.com>
2020-07-03 21:03:59 -0700

    Update SPEC.py

--- SPEC.py
+++ SPEC.py
@@ -1,16 +1,15 @@
 ##
 
-import http.client
-import mimetypes
-conn = http.client.HTTPSConnection("imdb-api.com", 443)
-payload = ''
-headers = {}
+import requests
 
-## https://imdb-api.com/en/API/SearchMovie/k_16w9K5sV/(1984)
-
-conn.request("GET", "/en/API/SearchMovie/k_16w9K5sV/1984", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
-
+## https://imdb-api.com/en/API/SearchMovie/k_16w9K5sV/(1984) 
+url = "https://imdb-api.com/en/API/SearchMovie/k_16w9K5sV/(1984)"
+##url = "https://imdb-api.com/en/API/Title/k_1234567/tt1832382"
+ 
+payload = {}
+headers= {}
+ 
+response = requests.request("GET", url, headers=headers, data = payload)
+ 
+print(response.text.encode('utf8'))
 
